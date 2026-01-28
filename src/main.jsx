9573d9c49271b465b307ead95ede03b147c12c19
Sebastian mardones <se.mardones@duouc.cl>
2026-01-27 21:40:11 -0300

    Configurar GitHub Pages y carta completa

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./styles.css"; // si tu archivo se llama style.css, cámbialo
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
